7b51dbdd35462b655accf78a4a9cd27611219d48
dickinsm <dickinsm@localhost>
2009-06-18 16:26:22 +0100

    Test suite for pympfr module

---
+++ test_pympfr.py
@@ -0,0 +1,17 @@
+import unittest
+from pympfr import *
+
+class MpfrTests(unittest.TestCase):
+    def setUp(self):
+        self.x = Mpfr()
+        mpfr.mpfr_init(x)
+
+
+
+
+
+def test_main():
+    unittest.main()
+
+if __name__ == '__main__':
+    test_main()
