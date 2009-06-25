1390f835c1940441c6311e97b370df8e5fba27d8
dickinsm <dickinsm@newton.local>
2009-06-25 15:13:46 +0100

    Tweak setup.py description

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='bigfloat',
       version='0.1',
-      description='Python wrapper for MPFR arbitrary-precision floating-point library',
+      description='Arbitrary precision correctly-rounded floating point arithmetic, via the MPFR library',
       author='Mark Dickinson',
       author_email='dickinsm@gmail.com',
       url='http://www.mpfr.org',
