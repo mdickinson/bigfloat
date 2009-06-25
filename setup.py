2e72074a9ec0c44825bcedb09dc361c977fec26c
dickinsm <dickinsm@newton.local>
2009-06-25 13:51:56 +0100

    Add setup script for distutils

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='BigFloat',
+      version='0.1',
+      description='Arbitrary-precision floating-point arithmetic',
+      author='Mark Dickinson',
+      author_email='dickinsm@gmail.com',
+      py_modules=['bigfloat', 'bigfloat_config', 'pympfr'],
+      )
