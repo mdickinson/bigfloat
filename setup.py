c1442c770aa2779035808dc4de73aa34af8a4cfb
dickinsm <dickinsm@newton.local>
2009-07-16 22:31:10 +0100

    Reorganize setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
-setup(name='bigfloat',
-      version='0.1',
-      description='Arbitrary precision correctly-rounded floating point arithmetic, via MPFR',
-      long_description=
 
-"""The bigfloat module provides a Python wrapper for the MPFR library.
+DESCRIPTION="""\
+Arbitrary precision correctly-rounded floating point arithmetic, via MPFR.\
+"""
+
+LONG_DESCRIPTION="""\
+The bigfloat module provides a Python wrapper for the MPFR library.
 The MPFR library is a well-known, well-tested C library for
 arbitrary-precision binary floating point reliable arithmetic; it's
 already used by a wide variety of projects, including GCC and SAGE.
@@ -24,10 +25,18 @@ Features:
  - full support for emulating IEEE 754 arithmetic in any of the IEEE binary
    interchange formats described in IEEE 754-2008.  Infinities, NaNs,
    signed zeros, and subnormals are all supported.
+"""
 
-""",
-      author='Mark Dickinson',
-      author_email='dickinsm@gmail.com',
-      url='http://www.mpfr.org',
-      packages=['bigfloat'],
-      )
+
+
+if __name__ == "__main__":
+
+    setup(name='bigfloat',
+          version='0.1',
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          author='Mark Dickinson',
+          author_email='dickinsm@gmail.com',
+          url='http://www.mpfr.org',
+          packages=['bigfloat'],
+          )
