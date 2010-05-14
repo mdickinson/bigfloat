a01f368a27539ab7b7bc35f18388b24d6ce39aea
Mark Dickinson <dickinsm@gmail.com>
2010-05-14 16:26:12 +0100

    Use platform.architecture rather than sys.maxsize to determine whether we're on a 64-bit platform;  sys.maxsize doesn't exist in Python <= 2.5.

---
+++ Makefile
@@ -0,0 +1,6 @@
+clean:
+	-rm -fr build/
+	-rm -fr Doc/_build/
+	-rm -fr bigfloat/*.pyc
+	-rm -fr bigfloat/examples/*.pyc
+	-rm -fr bigfloat/test/*.pyc
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import distutils.sysconfig
 import distutils.ccompiler
 import os.path
 import sys
+import platform
 
 DESCRIPTION="""\
 Arbitrary precision correctly-rounded floating point arithmetic, via MPFR.\
@@ -96,10 +97,11 @@ class build_config(Command):
             library_dirs.append(os.path.join(prefix, 'lib'))
 
         # standard locations
-        if sys.maxsize == 2**63-1:
+        if platform.architecture()[0] == '64bit':
             library_dirs.extend(['/usr/local/lib64', '/usr/lib64'])
-        elif sys.maxsize == 2**31-1:
+        else:
             library_dirs.extend(['/usr/local/lib32', '/usr/lib32'])
+
         library_dirs.append('/usr/local/lib')
         library_dirs.append('/usr/lib')
 
