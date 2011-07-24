aff0ccd550aaaaac52a33fff8f269537caaae6f7
Mark Dickinson <mdickinson@enthought.com>
2011-07-24 16:23:10 +0100

    Use __builtin__ module instead of creating builtin_* names.

--- mpfr_cython/bigfloat/core.py
+++ mpfr_cython/bigfloat/core.py
@@ -19,6 +19,7 @@
 
 from __future__ import with_statement  # for Python 2.5
 
+import __builtin__
 import sys as _sys
 import contextlib as _contextlib
 
@@ -180,14 +181,6 @@ def mpfr_get_str2(rop, base, ndigits, rounding_mode):
 ###############################################################################
 # Context manager to give an easy way to change emin and emax temporarily.
 
-# builtin abs, max, min and pow functions are shadowed by BigFloat
-# max, min and pow functions later on; keep a copy of the builtin
-# functions for later use
-_builtin_abs = abs
-_builtin_max = max
-_builtin_min = min
-_builtin_pow = pow
-
 try:
     DBL_PRECISION = _sys.float_info.mant_dig
 except AttributeError:
@@ -235,8 +228,8 @@ except AttributeError:
 EMAX_MAX = mpfr.MPFR_EMAX_DEFAULT
 EMIN_MIN = mpfr.MPFR_EMIN_DEFAULT
 
-EMAX_MIN = _builtin_max(mpfr.MPFR_EMIN_DEFAULT, mpfr.mpfr_get_emax_min())
-EMIN_MAX = _builtin_min(mpfr.MPFR_EMAX_DEFAULT, mpfr.mpfr_get_emin_max())
+EMAX_MIN = __builtin__.max(mpfr.MPFR_EMIN_DEFAULT, mpfr.mpfr_get_emax_min())
+EMIN_MAX = __builtin__.min(mpfr.MPFR_EMAX_DEFAULT, mpfr.mpfr_get_emin_max())
 
 mpfr.mpfr_set_emin(EMIN_MIN)
 mpfr.mpfr_set_emax(EMAX_MAX)
@@ -252,7 +245,7 @@ _bit_length_correction = {
 
 def _bit_length(n):
     """Bit length of an integer"""
-    hex_n = '%x' % _builtin_abs(n)
+    hex_n = '%x' % __builtin__.abs(n)
     return 4 * len(hex_n) - _bit_length_correction[hex_n[0]]
 
 
@@ -505,9 +498,9 @@ class BigFloat(object):
                                 "specified except when converting "
                                 "from a string")
             if isinstance(value, float):
-                precision = _builtin_max(DBL_PRECISION, PRECISION_MIN)
+                precision = __builtin__.max(DBL_PRECISION, PRECISION_MIN)
             elif isinstance(value, (int, long)):
-                precision = _builtin_max(_bit_length(value), PRECISION_MIN)
+                precision = __builtin__.max(_bit_length(value), PRECISION_MIN)
             elif isinstance(value, BigFloat):
                 precision = value.precision
             else:
@@ -878,9 +871,9 @@ class BigFloat(object):
         # 2**64-1).
 
         if e >= 0:
-            n = int(digits, 16) * _builtin_pow(16, e, 2 ** 64 - 1)
+            n = int(digits, 16) * __builtin__.pow(16, e, 2 ** 64 - 1)
         else:
-            n = int(digits, 16) * _builtin_pow(2 ** 60, -e, 2 ** 64 - 1)
+            n = int(digits, 16) * __builtin__.pow(2 ** 60, -e, 2 ** 64 - 1)
         return hash(-n if negative else n)
 
     def __ne__(self, other):
