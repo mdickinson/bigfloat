8aa3c2fbd3a166d20917c5b933afda69c505af50
Mark Dickinson <mdickinson@enthought.com>
2011-07-25 19:58:26 +0100

    Update mpfr_strtofr docstring.

--- mpfr_cython/bigfloat/mpfr.pyx
+++ mpfr_cython/bigfloat/mpfr.pyx
@@ -194,13 +194,62 @@ def mpfr_get_str(int b, size_t n, Mpfr op not None, cmpfr.mpfr_rnd_t rnd):
 
 def mpfr_strtofr(Mpfr rop not None, bytes s, int base, cmpfr.mpfr_rnd_t rnd):
     """
-    Read a floating-point number from a string s.
+    Read a floating-point number from a string.
 
-    Returns a pair (ternary, endindex), where:
+    Read a floating-point number from a string s in base base, rounded in
+    the direction rnd; base must be either 0 (to detect the base, as described
+    below) or a number from 2 to 62 (otherwise the behavior is undefined).
 
-      - ternary is the usual ternary return value
-      - endindex gives the index that points to the character of s
-        just after the valid data.
+    If s starts with valid data, the result is stored in rop and the function
+    returns a pair (ternary, endindex) where ternary is the usual ternary
+    return value and endindex gives the index of the character just after the
+    valid data.  Otherwise rop is set to zero (for consistency with strtod) and
+    endindex is 0.
+
+    Parsing follows the standard C strtod function with some extensions. After
+    optional leading whitespace, one has a subject sequence consisting of an
+    optional sign (+ or -), and either numeric data or special data. The
+    subject sequence is defined as the longest initial subsequence of the input
+    string, starting with the first non-whitespace character, that is of the
+    expected form.
+
+    The form of numeric data is a non-empty sequence of significand digits with
+    an optional decimal point, and an optional exponent consisting of an
+    exponent prefix followed by an optional sign and a non-empty sequence of
+    decimal digits. A significand digit is either a decimal digit or a Latin
+    letter (62 possible characters), with A = 10, B = 11, ..., Z = 35; case is
+    ignored in bases less or equal to 36, in bases larger than 36, a = 36, b =
+    37, ..., z = 61. The value of a significand digit must be strictly less
+    than the base. The decimal point can be either the one defined by the
+    current locale or the period (the first one is accepted for consistency
+    with the C standard and the practice, the second one is accepted to allow
+    the programmer to provide MPFR numbers from strings in a way that does not
+    depend on the current locale). The exponent prefix can be e or E for bases
+    up to 10, or @ in any base; it indicates a multiplication by a power of the
+    base. In bases 2 and 16, the exponent prefix can also be p or P, in which
+    case the exponent, called binary exponent, indicates a multiplication by a
+    power of 2 instead of the base (there is a difference only for base 16); in
+    base 16 for example 1p2 represents 4 whereas 1@2 represents 256. The value
+    of an exponent is always written in base 10.
+
+    If the argument base is 0, then the base is automatically detected as
+    follows. If the significand starts with 0b or 0B, base 2 is assumed. If the
+    significand starts with 0x or 0X, base 16 is assumed. Otherwise base 10 is
+    assumed.
+
+    Note: The exponent (if present) must contain at least a digit. Otherwise
+    the possible exponent prefix and sign are not part of the number (which
+    ends with the significand). Similarly, if 0b, 0B, 0x or 0X is not followed
+    by a binary/hexadecimal digit, then the subject sequence stops at the
+    character 0, thus 0 is read.
+
+    Special data (for infinities and NaN) can be @inf@ or
+    @nan@(n-char-sequence-opt), and if base <= 16, it can also be infinity,
+    inf, nan or nan(n-char-sequence-opt), all case insensitive. A
+    n-char-sequence-opt is a possibly empty string containing only digits,
+    Latin letters and the underscore (0, 1, 2, ..., 9, a, b, ..., z, A, B, ...,
+    Z, _). Note: one has an optional sign for all data, even NaN. For example,
+    -@nAn@(This_Is_Not_17) is a valid representation for NaN in base 17.
 
     """
     cdef char* endptr
