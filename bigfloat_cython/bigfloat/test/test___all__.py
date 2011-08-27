db7fda49af950b10f15cc4b2279acb296f1dc1e6
Mark Dickinson <mdickinson@enthought.com>
2011-08-27 14:58:01 +0100

    Add test for duplicate entries in __all__ in __init__.py

--- bigfloat_cython/bigfloat/test/test___all__.py
+++ bigfloat_cython/bigfloat/test/test___all__.py
@@ -1,8 +1,17 @@
+import collections
 import unittest
 
 class TestAll(unittest.TestCase):
     def test_from_import_star_on_bigfloat_package(self):
         exec "from bigfloat import *"
+
+    def test_duplicates(self):
+        # Check for duplicate entries in __all__.
+        from bigfloat import __all__
+        counts = collections.Counter(__all__)
+        duplicates = sorted(k for k, v in counts.items() if v > 1)
+        self.assertEqual(duplicates, [])
+
 
 if __name__ == '__main__':
     unittest.main()
